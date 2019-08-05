869e49e2e25c3ce3c448cc1f12717dcb20ca5915
Renee Sims <info@reneesoul.com>
2019-08-05 21:44:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ def all_holidays_with_bbq(holiday_hash)
   bbq_array = []
   holiday_hash.each do |season, holiday_name|
     holiday_name.each do |holiday, value|
-binding.pry
+# binding.pry
 
       if values == "BBQ" 
         bbq_array << holiday_name
